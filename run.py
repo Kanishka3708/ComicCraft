19070355e6656344d6dbfd2762ab21636b6aba66
Kanishka <unga email id>
2026-09-25 17:41:42 +0530

    Prepare ComicCraft for deployment

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ load_dotenv()
 from app import create_app
 app = create_app()
 if __name__ == '__main__':
-    app.run(host=os.getenv('HOST','127.0.0.1'), port=int(os.getenv('PORT','5000')), debug=os.getenv('FLASK_DEBUG','1') in ('1','true','True'))
+    app.run(host=os.getenv('HOST','0.0.0.0'), port=int(os.getenv('PORT','5000')), debug=os.getenv('FLASK_DEBUG','0') in ('1','true','True'))
